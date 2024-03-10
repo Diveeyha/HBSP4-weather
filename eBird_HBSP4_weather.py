599490f5bac9cf34c8b1fc4d22d787f84e2a5856
Diveeyha <67159522+Diveeyha@users.noreply.github.com>
2024-03-10 08:53:33 -0400

    Update eBird_HBSP4_weather.py

--- eBird_HBSP4_weather.py
+++ eBird_HBSP4_weather.py
@@ -8,17 +8,58 @@ import time
 from random import randint
 from time import sleep
 from datetime import datetime, timedelta
-from tzlocal import get_localzone
+from timezonefinder import TimezoneFinder
 
-def to_local(dt):
-    """From any timezone to local datetime - also cope with DST"""
-    localtime = time.localtime()
-    if localtime.tm_isdst:
-        utctime = time.gmtime()
-        hours_delta = timedelta(hours=(localtime.tm_hour - utctime.tm_hour))
-        dt = dt - hours_delta
 
-    return dt.replace(tzinfo=get_localzone())
+import pytz
+from typing import Optional
+from datetime import datetime
+
+
+
+
+
+class BritishTime(datetime):
+    timezone = pytz.timezone('America/New_York')
+
+    @classmethod
+    def dst(cls, dt: Optional[datetime] = None):
+        dt = dt if dt is not None else cls.now()
+        return cls.timezone.dst(dt)
+
+    # def get_timezone(lat, lng):
+    #     tf = TimezoneFinder()
+    #     tz = tf.timezone_at(lat=lat, lng=lng)
+    #     return tz
+
+def get_correct_time(timestamp):
+    # time_zone = BritishTime.get_timezone(lat, lng)
+    updated = BritishTime.fromtimestamp(timestamp)
+    return updated + updated.dst()
+
+
+
+
+# def to_local(dt, lat, lng):
+#
+#     """From any timezone to local datetime - also cope with DST"""
+#     localtime = time.localtime()
+#     if localtime.tm_isdst:
+#         utctime = time.gmtime()
+#         hours_delta = timedelta(hours=(localtime.tm_hour - utctime.tm_hour))
+#         dt = dt - hours_delta
+#
+#
+#     return dt.replace(tzname=tz)
+
+
+def is_dst(dt=None, timezone="UTC"):
+    if dt is None:
+        dt = datetime.utcnow()
+    timezone = pytz.timezone(timezone)
+    timezone_aware_date = timezone.localize(dt, is_dst=None)
+    return timezone_aware_date.tzinfo._dst.seconds != 0
+
 
 # @st.cache_data(ttl=60*10)
 # def get_NOAA(lat, lon):
@@ -34,13 +75,6 @@ def to_local(dt):
 #     NOAA_time = re.sub(r"-[0-9][0-9]:00", '', clean_str)
 #     return NOAA_hourly, NOAA_start, NOAA_time
 
-
-def is_dst(dt=None, timezone="UTC"):
-    if dt is None:
-        dt = datetime.utcnow()
-    timezone = pytz.timezone(timezone)
-    timezone_aware_date = timezone.localize(dt, is_dst=None)
-    return timezone_aware_date.tzinfo._dst.seconds != 0
 
 @st.cache_data(ttl=60*60)
 def get_merry_sky(lat, lon):
@@ -123,7 +157,7 @@ Precip: {precip2:.01f}%, {precip_amount2:.01f}in of {precip_type2}
 Rel Humidity: {rel_hum2}%
 Dewpoint: {dewpoint_F2:.1f}F ({dewpoint_C2:.1f}C)
 Visibility: {vis2:.01f}mi
-Updated: {to_local(time2)}'''
+Updated: {get_correct_time(adj_now)}'''
 
     # col1, col2 = st.columns(2)
     # with col1:
